31bfe817c4a23f11e390bd78e09d3bd6a39ed7b9
Francisco Santana <fsantan@sandia.gov>
2026-06-01 10:53:27 -0600

    Docs, formatting, SEL-3622 log-in test success

--- peat/modules/sel/sel_3622/__init__.py
+++ peat/modules/sel/sel_3622/__init__.py
@@ -1,6 +1,10 @@
 """
 SEL-3622 Security Gateway.
 
+The docs call this a "small form-factor" version of the 3620,
+but the differences seem to go deeper than calling it the
+3620's dwarf brother.
+
 Authors
 
 - Francisco Santana
@@ -8,7 +12,7 @@ Authors
 
 from peat import DeviceData, DeviceModule, IPMethod, exit_handler
 
-from .sel_http import SELHTTP
+from ..sel_http import SELHTTP
 
 
 class SEL3622(DeviceModule):
@@ -22,43 +26,67 @@ class SEL3622(DeviceModule):
     vendor_id = "SEL"
     vendor_name = "Schweitzer Engineering Laboratories"
     brand = "SEL"
-    module_aliases = [ 'sel-3622', '3622', 'sel-3620-slim', '3620-slim' ]
-    default_options = {
-        'web': {
-            'user': 'admin',
-            'pass': 'IAmAdmin',
-            'users': []
-        }
-    }
+    module_aliases = ["sel-3622", "3622", "sel-3620-slim", "3620-slim"]
+    default_options = {"web": {"user": "admin", "pass": "IAmAdmin!1", "users": []}}
 
-    def _verify_http(self, dev: DeviceData) -> bool:
+    @classmethod
+    def _verify_http(cls, dev: DeviceData) -> bool:
         """
         Validate that the device is an SEL-3622 via its HTTPS web interface
         """
-        self.log.info(f"SEL/3622: Verifying {dev.ip} via HTTP")
+        cls.log.info(f"SEL/3622: Verifying {dev.ip} via HTTPS")
 
         # TODO: perform validation
 
-        return False
+        port = dev.options["https"]["port"]
+        timeout = dev.options["https"]["timeout"]
 
-    # Override?
-    def _pull(self, dev: DeviceData) -> bool:
-        self.log.info(f"SEL/3622: Pulling information")
+        cls.log.debug(f"Verifying on port {port} with timeout {timeout}")
+
+        session = SELHTTP(dev.ip, port, timeout)
+        logged_in = False
+
+        user = None
+        passwd = None
+
+        if dev._cache.get("verified_web_user") and dev._cache.get("verified_web_pass"):
+            user = dev._cache["verified_web_user"]
+            passwd = dev._cache["verified_web_pass"]
+        else:
+            user = cls.default_options["web"]["user"]
+            passwd = cls.default_options["web"]["pass"]
+
+        cls.log.debug(f"Attempting log-in as {user}/{passwd}")
+        logged_in = session.login_3622(user, passwd)
+
+        if logged_in:
+            cls.log.info("Success! This device is an SEL-3622!")
+        else:
+            cls.log.error("Failure!")
+
+        session.logout_3622()
+
+        return logged_in
+
+    @classmethod
+    def _pull(cls, dev: DeviceData) -> bool:
+        cls.log.info(f"SEL/3622: Pulling information")
 
         # TODO: pull
 
         return False
 
+
 # This seems to list the methods to be used to perform validation
 SEL3622.ip_methods = [
     IPMethod(
-        name="sel_3622_web_fingerprint",
+        name="Perform a Web fingerprint (SEL-3622)",
         description=str(SEL3622._verify_http.__doc__).strip(),
         type="unicast_ip",
         identify_function=SEL3622._verify_http,
         default_port=443,
         protocol="https",
-        reliability=5, # TODO: Determine value
+        reliability=5,  # TODO: Determine value
         transport="tcp",
     )
 ]
