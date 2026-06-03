c0c72fd7aac301d687ce7914c8c51ab922fba0f8
Francisco Santana <fsantan@sandia.gov>
2026-06-03 10:07:26 -0600

    Attempted to fix FileManagement retrieval
    Still doesn't work, but these changes *should* be useful when this gets to work.

--- peat/modules/sel/sel_3622/FileManagement.py
+++ peat/modules/sel/sel_3622/FileManagement.py
@@ -4,12 +4,37 @@ Get data from /FileManagement.sel.
 Author: Francisco Santana <fsantan@sandia.gov>
 """
 
+from copy import copy
 from dataclasses import dataclass
 from time import gmtime, strftime, time
+from typing import Any, Final
 
 from bs4.element import Tag
 
+from .endpoints import ENDPOINTS
 from .http import HTTP3622
+
+HASH_ID: Final[str] = "display_systemSettingsExportHash"
+FORM_CONTENT: Final[dict[str, Any]] = {
+    "fileUploadType": "firmwareFile",
+    "JAVASCRIPT": "True",
+    "MAX_FILE_SIZE": "125000000",
+    "uploadedfile": "",
+    "t": "",
+    "Password": "",
+    "PasswordConfirm": "",
+    "submit": "",
+}
+
+
+def form_generate(password: str, token: str):
+    result = copy(FORM_CONTENT)
+    result["Password"] = password
+    result["PasswordConfirm"] = password
+    result["t"] = token
+    result["submit"] = "Generate"
+
+    return result
 
 
 @dataclass
@@ -19,70 +44,103 @@ class SystemSettings:
     time: str
 
 
-def queue_system_settings(http: HTTP3622) -> bool:
-    """
-    Queue the generation of the system settings file.
-    """
-
-    http.log.info("Preparing a configuration file snapshot...")
-
-    response = http.post(
-        http.endpoint("filesystem"),
-        data={
-            "Password": "Peater!1",
-            "PasswordConfirm": "Peater!1",
-            "submit": "Generate",
-        },
-    )
+def get_hash(http: HTTP3622) -> str | None:
+    response = http.get(ENDPOINTS["filesystem"], use_cache=False)
 
     if not response:
         http.log.error("No response")
-        return False
+        return None
 
     if response.status_code != 200:
-        http.log.error("Could not query setting file creation")
-        return False
+        http.log.error("Error loading page")
+        return None
 
     soup = http.gen_soup(response.text)
-    message = soup.find("div", {"id": "formMessage"})
 
-    if (
-        not isinstance(message, Tag)
-        or "System Settings file is being generated." not in message.get_text()
-    ):
-        http.log.error("Could not initiate settings file generation")
-        return False
+    old_hash = soup.find("span", {"id": HASH_ID})
 
-    http.log.info("Sent a generation request.")
-
-    # TODO: find a suitable way of awaiting the completion of this task.
-    # Threading?
-
-    return True
-
-
-def query_system_settings(http: HTTP3622) -> SystemSettings | None:
-    """
-    Query the status of the system settings file.
-
-    Will download it if it is ready.
-    """
-
-    response = http.post(
-        http.endpoint("filesystem"),
-        data={"Password": None, "PasswordConfirm": None, "submit": "Export"},
-        headers={"Referrer": http.endpoint("filesystem")},
-    )
-
-    if not response:
-        http.log.error("No response")
+    if not isinstance(old_hash, Tag):
+        http.log.error("Could not get old hash log")
         return None
 
-    if response.status_code != 200:
-        http.log.error("Could not pull file")
-        return None
-
-    raise NotImplementedError()
+    return old_hash.get_text(strip=True)
 
 
-__all__ = ["queue_system_settings", "query_system_settings", "SystemSettings"]
+class SystemSettingsPoller:
+    """
+    Hanles queueing and polling the system settings file
+    """
+
+    http: HTTP3622
+    old_hash: str
+
+    def __init__(self, http: HTTP3622):
+        self.http = http
+        self.old_hash = ""
+
+    def queue(self) -> bool:
+        """
+        Queue the generation of the system settings file.
+        """
+
+        self.http.log.info("Preparing a configuration file snapshot...")
+
+        old_hash = get_hash(self.http)
+
+        if not old_hash:
+            return False
+
+        response = self.http.post(
+            self.http.endpoint("filesystem"),
+            files=form_generate("Peater!1", self.http.session_id),
+            headers={"Referer": f"https://{self.http.ip}/{ENDPOINTS['filesystem']}"},
+        )
+
+        if not response:
+            self.http.log.error("No response")
+            return False
+
+        if response.status_code != 200:
+            self.http.log.error("Could not query setting file creation")
+            return False
+
+        soup = self.http.gen_soup(response.text)
+        message = soup.find("div", {"id": "formMessage"})
+
+        if (
+            not isinstance(message, Tag)
+            or "System Settings file is being generated." not in message.get_text()
+        ):
+            self.http.log.error("Could not initiate settings file generation")
+            return False
+
+        self.http.log.info("Sent a generation request.")
+        self.old_hash = old_hash
+
+        return True
+
+    def query(self) -> SystemSettings | None:
+        """
+        Query the status of the system settings file.
+
+        Will download it if it is ready. Returns `None` if it's not ready.
+        """
+
+        response = self.http.post(
+            self.http.endpoint("filesystem"),
+            data={"Password": None, "PasswordConfirm": None, "submit": "Export"},
+            headers={"Referrer": self.http.endpoint("filesystem")},
+        )
+
+        if not response:
+            self.http.log.error("No response")
+            return None
+
+        if response.status_code != 200:
+            self.http.log.error("Could not pull file")
+            return None
+
+        raise NotImplementedError()
+
+
+__all__ = ["SystemSettingsPoller", "SystemSettings"]
