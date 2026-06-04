ff95b397d48ffa6116ad13c416af564a6e4f2e64
Francisco Santana <fsantan@sandia.gov>
2026-06-04 11:37:28 -0600

    Improved doc to define the purpose of the submodule

--- peat/modules/sel/sel_3622/pull/__init__.py
+++ peat/modules/sel/sel_3622/pull/__init__.py
@@ -1,8 +1,17 @@
 """
 Pull methods for different endpoints.
 
+Ideally, scripts involved in pulling data from the device should go
+in this directory, and a singular function that returns a dictionary
+should be exported from each module.
+
+The prototype of each function should be:
+    (log: Logger, dev: DeviceData, session: HTTP3622) -> dict[str, Any]
+
+Raise exceptions where the pull fails.
+
 Authors:
     - Francisco Santana <fsantan@sandia.gov>
 """
 
-from FileManagement import pull_system_settings
+from FileManagement import pull_file_management
