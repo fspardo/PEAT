8b9b28058c432c1cc329d6bf3d55f6a9ac8c5f14
Francisco Santana <fsantan@sandia.gov>
2026-06-09 10:28:02 -0600

    Global config, nics

--- peat/modules/sel/sel_3622/pull/NetworkSettings.py
+++ peat/modules/sel/sel_3622/pull/NetworkSettings.py
@@ -16,28 +16,81 @@ from ..http import HTTP3622
 
 
 def get_global_cfg(
-    soup: BeautifulSoup,
-) -> dict[Literal["hostname", "domain", "gateway"], Any]:
-    return {}
+    soup: BeautifulSoup, session: HTTP3622
+) -> dict[Literal["hostname", "domain", "gateway", "dhcp_gateway"], Any]:
+    """
+    Retrieve the global configuration
+    """
+    hostname = soup.find("td", {"id": "display_Hostname"})
+    domain = soup.find("td", {"id": "display_DomainName"})
+    gateway = soup.find("td", {"id": "display_Gateway"})
+
+    assert isinstance(hostname, Tag)
+    hostname = hostname.get_text("", True)
+    assert isinstance(domain, Tag)
+    domain = domain.get_text("", True)
+    assert isinstance(gateway, Tag)
+    gateway = gateway.get_text("", True)
+
+    result = {}
+    if len(hostname) > 0:
+        result["hostname"] = hostname
+    if len(domain) > 0:
+        result["domain"] = domain
+    if len(gateway) > 0:
+        result["gateway"] = gateway
+
+    # TODO: see if it is possible to somehow determine what DHCP gateway to use other than the manual default gateway
+
+    return result
 
 
 def get_nics(
-    soup: BeautifulSoup,
-) -> dict[str, dict[Literal["enabled", "configured"], bool]]:
-    return {}
+    soup: BeautifulSoup, _: HTTP3622
+) -> dict[str, dict[Literal["status", "configured"], bool]]:
+    """
+    Retrieve NIC status
+    """
+    table = soup.find("table", {"id": "NetworkInterfaces"})
+    if not isinstance(table, Tag):
+        raise Exception("Could not get Ethernet configuration table")
+
+    entries = table.find_all("img")
+
+    result = {}
+
+    for img in entries:
+        # Get information from the alt text, as it says practically everything
+        assert isinstance(img, Tag)  # Sanity check
+        alt = img.attrs["alt"]
+        assert isinstance(alt, str)  # Sanity check
+        data = alt.split(" - ")
+
+        result[data[0]] = {"status": data[1], "configured": data[2]}
+
+    return result
 
 
-def get_addresses(
-    soup: BeautifulSoup, session: HTTP3622
-) -> dict[
-    str, dict[Literal["interface", "ip", "vlan", "webserver", "dhcp_client"], Any]
+def get_addresses(soup: BeautifulSoup, session: HTTP3622) -> dict[
+    str,
+    dict[
+        Literal["interface", "ip", "vlan", "webserver", "dhcp_client"],
+        Any,
+    ]
+    | str,
 ]:
-    return {}
+
+    return {"implemented": "not"}
 
 
 def pull_network_settings(dev: DeviceData, session: HTTP3622) -> dict[str, Any]:
     """
     Pull the configuration under /NetworkSettings.sel
+
+    | Field                                | Description                                                             |
+    |--------------------------------------|-------------------------------------------------------------------------|
+    | `network`                            | Root container of the configuration                                     |
+
     """
 
     response = session.get_endpoint("network_settings")
@@ -52,8 +105,8 @@ def pull_network_settings(dev: DeviceData, session: HTTP3622) -> dict[str, Any]:
     soup = session.gen_soup(response.text)
     return {
         "network": {
-            "global": get_global_cfg(soup),
-            "nics": get_nics(soup),
+            "global": get_global_cfg(soup, session),
+            "nics": get_nics(soup, session),
             "addresses": get_addresses(soup, session),
         }
     }
