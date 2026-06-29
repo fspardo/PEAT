6897aa77876f7118181044cbcec8a5f90c2d3593
Francisco Santana <fsantan@sandia.gov>
2026-06-29 10:31:32 -0600

    Fixed documentation

--- peat/modules/sel/sel_3622/pull/LDAP.py
+++ peat/modules/sel/sel_3622/pull/LDAP.py
@@ -23,21 +23,21 @@ def pull_ldap_settings(dev: DeviceData, session: HTTP3622) -> dict[str, Any]:
 
     | Field                                    | Description                                             |
     |------------------------------------------|---------------------------------------------------------|
-    | `users.ldap`                             | Container for parsed data                               |
-    | `users.ldap.config`                      | Global configuration                                    |
-    | `users.ldap.config.enabled`              | Whether LDAP is enabled                                 |
-    | `users.ldap.config.user_id_attribute`    | Attribute to use for the user's username                |
-    | `users.ldap.config.sync_interval`        | How often (in hours) to sync with the LDAP server       |
-    | `users.ldap.config.search_base`          | <UNKNOWN>                                               |
-    | `users.ldap.config.bind_dn`              | <UNKNOWN>                                               |
-    | `users.ldap.servers`                     | A list of servers to search                             |
-    | `users.ldap.servers.[hostname]`          | The hostname of the server entry                        |
-    | `users.ldap.servers.[hostname].priority` | The priority of this server                             |
-    | `users.ldap.servers.[hostname].port`     | The port number to connect to                           |
-    | `users.ldap.attributes`                  | The list of attributes to read from the LDAP server     |
-    | `users.ldap.attributes.[label]`          | The attribute label, mapped to its LDAP attribute       |
-    | `users.ldap.group_mappings`              | A list of group mappings.                               |
-    | `users.ldap.group_mappings.[role]`       | The name of the role (tech/admin), with its list of DNs |
+    | ` ldap`                                  | Container for parsed data                               |
+    | ` ldap.config`                           | Global configuration                                    |
+    | ` ldap.config.enabled`                   | Whether LDAP is enabled                                 |
+    | ` ldap.config.user_id_attribute`         | Attribute to use for the user's username                |
+    | ` ldap.config.sync_interval`             | How often (in hours) to sync with the LDAP server       |
+    | ` ldap.config.search_base`               | <UNKNOWN>                                               |
+    | ` ldap.config.bind_dn`                   | <UNKNOWN>                                               |
+    | ` ldap.servers`                          | A list of servers to search                             |
+    | ` ldap.servers.[hostname]`               | The hostname of the server entry                        |
+    | ` ldap.servers.[hostname].priority`      | The priority of this server                             |
+    | ` ldap.servers.[hostname].port`          | The port number to connect to                           |
+    | ` ldap.attributes`                       | The list of attributes to read from the LDAP server     |
+    | ` ldap.attributes.[label]`               | The attribute label, mapped to its LDAP attribute       |
+    | ` ldap.group_mappings`                   | A list of group mappings.                               |
+    | ` ldap.group_mappings.[role]`            | The name of the role (tech/admin), with its list of DNs |
     """
     logger.debug("Pulling page...")
     response = session.get_endpoint("ldap_settings")
