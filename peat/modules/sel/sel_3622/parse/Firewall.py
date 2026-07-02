dcd7c0d1b1c11ef139382cb53443b1a2d2fd842b
Francisco Santana <fsantan@sandia.gov>
2026-07-02 15:02:24 -0600

    Fixed issues

--- peat/modules/sel/sel_3622/parse/Firewall.py
+++ peat/modules/sel/sel_3622/parse/Firewall.py
@@ -18,37 +18,38 @@ def parse_rule(tag: Tag) -> dict[str, Any]:
     """Parse a row containing a rule"""
     result = {}
 
-    strs = [
-        "globalOrder",
-        "ruleInterface",
-        "StatusName",
-        "sourcePort",
-        "descProtocolRule",
-        "destPort",
-    ]
+    try:
+        strs = [
+            "globalOrder",
+            "ruleInterface",
+            "StatusName",
+            "sourcePort",
+            "descProtocolRule",
+            "destPort",
+        ]
 
-    def getk(tag: Tag, k: str) -> list[str]:
-        v = tag.find("td", {"class": k})
-        assert isinstance(v, Tag)
-        v = v.get("value")
-        assert isinstance(v, str)
-        return v.splitlines()
+        def getk(tag: Tag, k: str) -> list[str]:
+            v = tag.find("td", {"class": k})
+            assert isinstance(v, Tag)
+            return v.get_text("\n", True).splitlines()
 
-    values = {k: getk(tag, k) for k in strs}
+        values = {k: getk(tag, k) for k in strs}
 
-    result["order"] = values["globalOrder"][0].strip()
-    result["interface"] = values["ruleInterface"][0].strip()
-    result["name"] = values["StatusName"][0].strip()
-    result["status"] = values["StatusName"][1].strip()
-    result["source_address"] = values["sourcePort"][0].strip()
-    result["source_port"] = values["sourcePort"][1].strip()
-    # Just in case the description field ends up supporting line breaks (the text box used to fill it does)
-    result["description"] = " ".join(values["descProtocolRule"][:-1]).strip()
-    protorule = values["descProtocolRule"][-1].split("\t")
-    result["protocol"] = protorule[0].strip()
-    result["action"] = protorule[-1].strip()
-    result["destination_address"] = values["destPort"][0].strip()
-    result["destination_port"] = values["destPort"][1].strip()
+        result["order"] = values["globalOrder"][0].strip()
+        result["interface"] = values["ruleInterface"][0].strip()
+        result["name"] = values["StatusName"][0].strip()
+        result["status"] = values["StatusName"][1].strip()
+        result["source_address"] = values["sourcePort"][0].strip()
+        result["source_port"] = values["sourcePort"][1].strip()
+        # Just in case the description field ends up supporting line breaks (the text box used to fill it does)
+        result["description"] = " ".join(values["descProtocolRule"][:-1]).strip()
+        protorule = values["descProtocolRule"][-1].split("\u00a0")
+        result["protocol"] = protorule[0].strip()
+        result["action"] = protorule[-1].strip()
+        result["destination_address"] = values["destPort"][0].strip()
+        result["destination_port"] = values["destPort"][1].strip()
+    except:
+        logger.warning("Failed to parse row")
 
     return result
 
@@ -80,10 +81,8 @@ def parse_rules(soup: BeautifulSoup) -> dict[str, Any]:
 
     result.update(parse_general_rules(soup))
 
-    tbl = soup.get("table", {"id": "firewallRules"})
-    assert isinstance(tbl, Tag)
     result["rules"] = [
-        parse_rule(row) for row in tbl.find_all("tr", {"class": ["odd", "even"]})
+        parse_rule(row) for row in soup.find_all("tr", {"class": ["odd", "even"]})
     ]
 
     return {"firewall": result}
