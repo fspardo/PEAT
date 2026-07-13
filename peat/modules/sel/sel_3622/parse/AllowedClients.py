61ad082ed9a3fc879dcef233c26533889404586f
Francisco Santana <fsantan@sandia.gov>
2026-07-13 17:28:27 -0600

    Basic parsing

--- peat/modules/sel/sel_3622/parse/AllowedClients.py
+++ peat/modules/sel/sel_3622/parse/AllowedClients.py
@@ -18,4 +18,31 @@ def parse_clients(soup: BeautifulSoup) -> dict[str, Any]:
     """Performs a basic parse of the table contents in the main page"""
     result = {}
 
+    table = soup.find("table", {"id": "SharedClients"})
+    assert isinstance(table, Tag)
+
+    rows = table.find_all("tr", {"class": ["even", "odd"]})
+
+    for row in rows:
+        assert isinstance(row, Tag)
+
+        alias = row.find("td", {"class": "Alias"})
+        assert isinstance(alias, Tag)
+        alias = alias.get_text("", True)
+
+        address = row.find("td", {"class": "IP"})
+        assert isinstance(address, Tag)
+        address = address.get_text("", True)
+
+        description = row.find("td", {"class": "Description"})
+        assert isinstance(description, Tag)
+        description = description.get_text("", True)
+
+        types = row.find("td", {"class": "Types"})
+        assert isinstance(types, Tag)
+        imgs = types.find_all("img")
+        types = types.get_text(";").split(";")
+
+        result[alias] = {"address": address, "description": description, "types": types}
+
     return result
