19f4a861b6c099cb800ca87518fb72f0655cc540
Francisco Santana <fsantan@sandia.gov>
2026-07-14 09:37:53 -0600

    SSH Host Key parsing

---
+++ peat/modules/sel/sel_3622/parse/SSHHostKey.py
@@ -0,0 +1,34 @@
+"""
+Parse data from /SSHHostKey.sel.
+
+Author: Francisco Santana <fsantan@sandia.gov>
+"""
+
+from pathlib import Path
+from typing import Any, Final
+
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+from loguru import logger
+
+from peat import DeviceData
+
+FORMS = {
+    "dsa_pubkey": "pubSSH",
+    "rsa_pubkey": "pubRSA",
+}
+
+
+def parse_host_keys(soup: BeautifulSoup) -> dict[str, Any]:
+    """Performs a basic parse of the table contents in the main page"""
+    result = {}
+
+    for f in FORMS:
+        form = soup.find("form", {"id": FORMS[f]})
+        assert isinstance(form, Tag)
+        pre = form.find("pre")
+        assert isinstance(pre, Tag)
+
+        result[f] = pre.get_text("\n", True).splitlines()
+
+    return result
---
+++ peat/modules/sel/sel_3622/pull/SSHHostKey.py
@@ -0,0 +1,46 @@
+"""
+Get data from /SSH_Host_Key.sel.
+
+Author: Francisco Santana <fsantan@sandia.gov>
+"""
+
+from typing import Any, Literal
+
+from bs4 import BeautifulSoup
+from bs4.element import ResultSet, Tag
+from loguru import logger
+
+from peat import DeviceData
+
+from ..http import HTTP3622
+from ..parse.SSHHostKey import parse_host_keys
+
+
+def pull_host_keys(dev: DeviceData, session: HTTP3622) -> dict[str, Any]:
+    """
+    Pull the configuration under /SSH_Host_Key.sel
+
+    | Field                  | Description                     |
+    |------------------------|---------------------------------|
+    | `host_keys`            | Root container                  |
+    | `host_keys.dsa_pubkey` | The DSA public key for the host |
+    | `host_keys.rsa_pubkey` | The RSA public key for the host |
+    """
+
+    logger.debug("Pulling page...")
+    response = session.get_endpoint("ssh_host_key")
+
+    if not response:
+        raise Exception("No response")
+    if response.status_code != 200:
+        raise Exception(f"Got non-200 status: {response.status_code}")
+    if response.history:
+        raise Exception("Redirected")
+
+    soup = session.gen_soup(response.text)
+
+    logger.debug("Parsing page...")
+
+    result = parse_host_keys(soup)
+
+    return {"host_keys": result}
